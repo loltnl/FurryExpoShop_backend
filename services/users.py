5a155af75bbe465ae0360991c10c8ccbedf2501b
myqbq <3167237080@qq.com>
2023-08-17 09:00:40 +0000

    拉完了，睡觉，新增一点代码

--- services/users.py
+++ services/users.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from fastapi import Query
+
+from typing import Annotated
 
 from plugins.gen_jwtdata import generate_jwt
 from plugins.gen_jwtdata import check_jwt
@@ -12,6 +15,7 @@ from pydantic import BaseModel
 from databases.users import ColUsers
 
 
+
 UserPage = APIRouter()
 
 
@@ -20,28 +24,45 @@ def GenerateCaptcha():
     return
 
 
-@UserPage.post("/register/byphone")
-def RegByphone():
-    return
+# @UserPage.post("/register/byphone")
+# def RegByphone():
+#     return
 
 
-@UserPage.post("/register/bywechat")
-def RegByWechat():
-    return
+# @UserPage.post("/register/bywechat")
+# def RegByWechat():
+#     return
+
+# H5端注册接口
+class RegisterByH5(BaseModel):
+    username: Annotated[str, Query(title="账号", min_length=3, max_length=16)]
+    phone: Annotated[str, Query(title="手机号", min_length=13, max_length=13)]
+    password: Annotated[str, Query(title="密码", min_length=8, max_length=20)]
+    retype_password: Annotated[str, Query(title="确认密码", min_length=8, max_length=20)]
+    captcha_jwt: Annotated[str, Query(title="人机验证码", min_length=6, max_length=6)]
+    
+
 
 # 登录接口
 class Login(BaseModel):
-    username: str
-    password: str
-    captcha: str
+    username: Annotated[str, Query(title="账号", min_length=3, max_length=16)]
+    password: Annotated[str, Query(title="密码", min_length=8, max_length=20)]
+    captcha: Annotated[str, Query(title="人机验证码", min_length=6, max_length=6)]
 
 @UserPage.post("/login")
 def UserLogin(params: Login):
     data = ColUsers().query(params.username)
     if data is None:
         return servicedata(ResQueryNotFound, msg="未找到该用户")
+    else:
+        return servicedata(ResNormalCheck)
     
     return 
+
+### !!!!调试接口用!!!! 生产环境请注释掉该段代码
+@UserPage.post("/login/direct_password_check")
+def DirectPasswordCheck():
+    return
 
 # JWT测试
 @UserPage.get("/testjwt/gen")
